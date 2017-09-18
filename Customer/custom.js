750766bd4bf76b43b6c007d21971d6bd607164e7
Raja Sekhar Sana <sanarajasekhar@gmail.com>
2017-09-18 14:20:26 +0530

    Add files via upload

--- Customer/custom.js
+++ Customer/custom.js
@@ -1,6 +1,61 @@
 var server = "http://mukthasis.com/boorugu/";
 $(function(){
 connection();
+$("#innbody").css("height",$( window ).height()-20);
+$("#order").load(server+"loadOrder.php?oid="+localStorage.getItem("Order"));
+$("#ordercreate").on('submit', function(e){
+    e.preventDefault();
+    $("#cby").val(localStorage.getItem("User"));
+     var fdata = $("#ordercreate").serialize();
+     $.ajax({
+        url:server+"ordercreate_customer.php",
+        data:fdata,
+        type:"post",
+        beforeSend:function(){
+          $("#ploader").show();
+        },
+        success: function(str){
+          $("#ploader").hide();
+          $("#smsg").show();
+          $("#ordercreate")[0].reset();
+          $("#submit").prop('disabled',false);
+        },
+        error: function(xhr){
+           alert('Request Status: ' + xhr.status + ' Status Text: ' + xhr.statusText + ' ' + xhr.responseText);
+        }
+     });
+  });
+
+
+$("#orders_customer tbody").html("");
+var url2 = server+"orders_customer.php?cid="+localStorage.getItem("User");
+$.getJSON(url2,function(data){
+   $.each(data.users, function(i,user){
+     var newRow=
+      "<tr onclick='showOrder(\""+user.OrderId+"\");'>"
+      +"<td>"+(i+1)+"</td>"
+      +"<td>"+user.OrderId+"</td>"
+      +"<td>"+user.CreateBy+"</td>"
+      +"<td>"+user.Division+"</td>"
+      +"<td>"+user.Customer+"</td>"
+      +"<td>"+user.Industry+"</td>"
+      +"<td align='right'>"+user.Amount+"</td>"
+      +"</tr>";
+      $(newRow).appendTo("#orders_customer tbody");
+   });
+});
+
+var url4 = server+"industries.php";
+$("#industryorder").html("");
+var newRow= "<option value=''>SELECT INDUSTRY</option>";
+$(newRow).appendTo("#industryorder");
+$.getJSON(url4,function(data){
+   $.each(data.users, function(i,indus){
+      var newRow=
+      "<option value='"+indus.Sno+"'>"+indus.Industry+"</option>";
+      $(newRow).appendTo("#industryorder");
+   });
+});
 
 });
 function connection(){
@@ -18,4 +73,46 @@ function logout(){
   localStorage.removeItem("Users");
   localStorage.removeItem("Role");
   location.href="../index.html";
+}
+
+function showOrderForm(inds){
+  if(inds != ''){
+    var fdata = {"inds":inds};
+   $.ajax({
+      url:server+"orderform.php",
+      data:fdata,
+      type:"post",
+      success: function(str){
+        $("#oform").html(str);
+      }
+   });
+  }
+}
+
+function changePassword(){
+  var npass = $("#npass").val();
+  var cnpass = $("#cnpass").val();
+  var fdata = {"npass":npass,"cnpass":cnpass,"user":localStorage.getItem("User"),"table":"customers"};
+  $.ajax({
+     url:server+"changePassword.php",
+     data:fdata,
+     type:"post",
+     beforeSend: function(){
+       $("#preloader").show();
+     },
+     success: function(str){
+       $("#preloader").hide();
+       if(str == "Success"){
+         $("#emsg").hide();
+         $("#smsg").show();
+       }else{
+         $("#emsg").show();
+       }
+     }
+  });
+}
+
+function showOrder(oid){
+localStorage.setItem("Order",oid);
+ location.href="ViewOrder.html";
 }
